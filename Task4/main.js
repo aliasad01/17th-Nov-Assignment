bad28df940d07e672ac3019db8436efa5f947f65
aliasad01 <111585256+aliasad01@users.noreply.github.com>
2022-11-16 08:07:46 -0800

    Add files via upload

---
+++ Task4/main.js
@@ -0,0 +1,7 @@
+function getOptions(){
+
+var list = document.getElementById("mySelect");
+let drop = list.options.length + " options";
+document.getElementById('numdrop').innerHTML = drop;
+alert(drop)
+}
